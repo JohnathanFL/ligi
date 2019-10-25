5ca48c5077c19c0d4bdb67e88741c86db06c4c76
JohnathanFL <jfl747076@gmail.com>
2019-10-25 11:42:56 -0500

    Beginning of the new(est) grammar

--- src/node.zig
+++ src/node.zig
@@ -1,99 +1,104 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const HashMap = std.AutoHashMap;
 const Tokens = @import("token.zig");
 const Token = Tokens.Token;
 const Tag = Tokens.Tag;
 
-pub const Op = union(enum) {
-    // voids are builtins
-
-    // Args: condition, captureToken, execIfTrue, execIfFalse
-    // For example:
-    // if(optVal) |val| {
-    // val = 1;
-    // }
-    // Becomes: (using Unlabeled {} as a List)
-    // {op: If, items: {optVal, Token{"val"}, {`=`, Token{"val"}, Token{1}}, Null}}
-    If: void,
-
-    // Args: condition, captureToken, execContinue, execDuring, execAfter
-    // For example: (Assuming i exists)
-    // while(i < 5) : (++i) {
-    //   print("Hi");
-    // } else {
-    //   print("Finished");
-    // }
-    // Becomes:
-    // {op: While, items: {
-    // {op: `<`, items: {i, 5}},
-    //  Null,
-    //  {op: `++`, items: Token{i}},
-    //  {op: Func(print), items: {Token{"Hi"}}},
-    //  {op: Func(print), items: {Token{"Finished"}}
-    //  }
-    // }
-    While: void,
-
-    // Args: list, captureToken, execDuring, execAfter
-    For: void,
-
-    // TODO: Do we need this one at all? The Value.Token ought to do it already.
-    // Take the literal value of the list (i.e a tuple or single value).
-    // Used to execute the full program.
-    Val: void,
-
-    // Make an immutable variable in current scope.
-    // Used for variables AND types and such.
-    // Think Zig's 'const'.
-    // Args: bindToken, type, val
-    Let: void,
-    // Make a mutable variable in current scope
-    // Args: bindToken, type, val
-    Var: void,
-    // Name of the function to execute.
-    // Functions that literally have the '`' characters surrounding them
-    // are operators. (e.g `+`). This way they can be overloaded
-    Func: []const u8,
-
-    pub fn fromTag(tag: Tag) Op {
-        switch(tag) {
-            .Let => return .Let,
-            .Var => return .Let,
-            .While => return .While,
-            .For => return .For,
-            .If => return .If,
-            else => {
-                std.debug.warn("ERROR: Attemped to convert {} to an op!\n", tag);
-                @panic("Invalid conversion.\n");
-            }
-        }
-    }
+pub const Call = struct {
+  func: Token,
+  args: ArrayList(*Expr),
 };
 
-pub const List = struct {
-    op: Op,
-    items: ArrayList(Value),
-
-    pub fn init(alloc: *Allocator) List {
-        return List{
-            .op = .Var,
-            .items = ArrayList(Value).init(alloc),
-        };
-    }
-
-    pub fn from(alloc: *Allocator, op: Op, items: []const Value) List {
-        var res = init(alloc);
-        res.op = op;
-        res.items.appendSlice(items);
-        return res;
-    }
+pub const Expr = union(enum) {
+  call: Call,
+  ifExpr: *IfExpr,
+  
+  val: Token,  
 };
 
-pub const Value = union(enum) {
-    // If the list is to be taken as is (like tuple (1, 2, 3)),
-    // then list.op == Op.Val
-    List: List,
-    Token: Token,
-    Null: void,
+pub const Stmt = union(enum) {
+  expr: Expr,
+  forStmt: ForStmt,
+  whileStmt: WhileStmt,
+  bindStmt: BindStmt,
+};
+
+// val is what can be returned by the block.
+// defaults to void
+pub const Block = struct {
+  symbols: HashMap([]const u8, *BindExpr),
+  stmts: ArrayList(*Stmt),
+  // If the block returns nothing, this will just get pointed to a global "nullexpr" thingy
+  val: *Expr
+};
+
+pub const IfCondBlock = struct {
+  cond: *Expr,
+  if_so: *Block,
+  capture_token: ?*BindStmt,
+}
+
+pub const IfExpr = struct {
+  // The value of the IfExpr is taken from the value of whichever block is evaluated as true.
+  // If used in a spot needing a value, then the else must be present.
+
+  // else gets handled as CondBlock{true, {...}}
+  conds: ArrayList(IfCondBlock),
+  finally: ?*Block,
+};
+
+pub const ForStmt = struct {
+  iterable: *Expr,
+  count_token: ?*BindStmt,
+  capture_token: ?*BindStmt,
+  loop: *Block, // Value must be void
+  finally: ?*Block,  
+
+  label: ?[]const u8,
+};
+
+pub const WhileStmt = struct {
+  cond: *Expr,
+  count_token: ?*BindStmt,
+  capture_token: ?*BindStmt,
+  loop: *Block,
+  finally: *Block,
+};
+
+pub const BindStmt = struct {
+  // Debugging values:
+  line: usize,
+  file_id: usize,  
+
+  is_var: bool,
+  binding: []const u8,
+  // If it is to be inferred, then there will be a special global "auto" type.
+  // "auto" would only be valid in 
+  typeof: *Type = &auto_type,
+  intitial: ?*Expr = null,
+};
+pub const Array = struct {
+  size: usize,
+  typeof: *Type
+};
+
+// Comparison with this should be on an address basis
+pub const auto_type = Type {.symbol = "____auto____"};
+pub const Type = union(enum) {
+  slice: *Type,
+  array: Array,
+  opt: *Type,
+  // Errors will all be dynamicly decided at compile time, so no need for an explicit error type.
+  err: *Type,
+  symbol: []const u8,
+  
+};
+
+pub const Lambda = struct {
+  is_pure: bool,
+  // The function's block's symbol table will have the same *BindStmts
+  args: ArrayList(*BindStmt),
+  ret: *Type,
 };
