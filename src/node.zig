9b779d389ec5b04c5e699d7bb410cfaf16f23989
JohnathanFL <jfl747076@gmail.com>
2019-11-26 16:23:48 -0600

    Begin the new new new grammar

--- src/node.zig
+++ src/node.zig
@@ -7,327 +7,87 @@ const Tokens = @import("token.zig");
 const Token = Tokens.Token;
 const Tag = Tokens.Tag;
 
-// There are 3 major types in the AST:
-//  Stmt: Broken into If/Loop/Bind. Can also point to a Block/Expr
-//  Block: Contains Stmts. May be labeled
-//  Expr: Produces a value (may be void). Broken into Literal/Symbol/Case/Call.
-//    A call is used for all operators. (e.g foo.bar => Call('.', 'foo', 'bar') )
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Stmt Stuff
-///////////////////////////////////////////////////////////////////////////////////////////////////
-pub const Stmt = union(enum) {
-  If: If,
-  /// Covers For and While
-  Loop: Loop,
-  Bind: Bind,
-  Block: *Block,
-  Expr: *Expr,
-
-  pub fn fromExpr(expr: *Expr, alloc: *Allocator) *Stmt {
-    var res = alloc.create(Stmt) catch unreachable;
-    res.* = Stmt { .Expr = expr };
-    return res;
-  }
-
-  pub fn fromBlock(block: *Block, alloc: *Allocator) *Stmt {
-    var res = alloc.create(Stmt) catch unreachable;
-    res.* = Stmt { .Block = block };
-    return res;
-  }
+pub const Block = struct {
+    interpret: enum {  
+      Struct,
+      Enum,
+      /// Zag's answer to generic interfaces
+      Concept,
+      /// Don't interpret it at all
+      Stream,
+      /// Completely normal mode: Just run each statement inside at runtime.
+      Block
+    },
+    stmts: ArrayList(*Stmt),
+    label: ?Token,
 };
 
-pub const IfArm = struct {
-  cond: *Expr,
-  val: *Block
+pub const Stmt = union(enum) {
+    ifStmt: *If,
+    loop: *Loop,
+    expr: *Expr,
+    bind: *Bind,
+};
+
+pub const Expr = union(enum) {
+    block: *Block,
+    call: *Call,
+    literal: Token,
+    symbol: Token,
+    case: *Case,
 };
 
 pub const If = struct {
-  cond: *Expr,
-  arms: ArrayList(IfArm),
-  default: ?*Block,
-  finally: ?*Block,
-
-  pub fn new(alloc: *std.mem.Allocator) *Stmt {
-    var res = alloc.create(Stmt) catch unreachable;
-    res.* = Stmt {
-      .If = If {
-        .cond = undefined,
-        .arms = ArrayList(IfArm).init(alloc),
-        .default = null,
-        .finally = null,
-      },
-    };
-    return res;
-  }
-
-  pub fn pushArm(self: *If, cond: *Expr, val: *Block) void {
-    self.arms.append(IfArm{ .cond = cond, .val = val}) catch unreachable;
-  }
+    arms: ArrayList(struct {
+      cond: *Expr,
+      then: *Block,
+    }),
+    default: ?*Block,
+    finally: ?*Block,
 };
 
-pub const LoopType = enum {
-  For, While, Infinite // Maybe
-};
 pub const Loop = struct {
-  ty: LoopType,
-  eval: *Expr, // Evaluated once for a for loop, each loop for a while
-  counter: ?*Bind, // Loop iteration number
-  capture: ?*Bind, // Capture from foreach or optional in a while
-  do: *Block,
-  finally: ?*Block, // Executed if the loop isn't broken
-
-  pub fn new(alloc: *Allocator, ty: LoopType) *Stmt {
-    var res = alloc.create(Stmt) catch unreachable;
-    res.* = Stmt {
-      .Loop = Loop {
-        .ty = ty,
-        .eval = undefined,
-        .counter = null,
-        .capture = null,
-        .do = undefined,
-        .finally = null
-      }
-    };
-    return res;
-  }
-
+    interpret: enum {
+        Infinite,
+        For,
+        While
+    },
+    capture: struct {
+        index: ?Token,
+        variable: ?struct {
+            ref: bool,
+            tok: Token,
+        },
+    },
+    body: *Block,
+    finally: ?*Block,
 };
 
 pub const Bind = struct {
-  mut: bool, // True for var, false for let
-  loc: Token,
-  ty: Type,
-  val: ?*Expr, // The default value
-
-  pub fn new(alloc: *Allocator, mut: bool) *Stmt {
-    var res = alloc.create(Stmt) catch unreachable;
-    res.* = Stmt {
-      .Bind = Bind {
-        .mut = mut,
-        .loc = undefined,
-        .ty = undefined,
-        .val = null,
-      }
-    };
-    return res;
-  }
+    interpret: enum {
+        Let,
+        Var,
+        Field,
+        Enum,
+    },
+    loc: Token,
+    ty: ?*Type,
+    /// If interpret === Let,Var, then default != null
+    default: ?*Expr,
 };
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Block Stuff
-///////////////////////////////////////////////////////////////////////////////////////////////////
-pub const Block = struct {
-  label: ?Token,
-  stmts: ArrayList(*Stmt),
-  binds: StringHashMap(*Bind),
-  parent: ?*Block,
-  pub fn new(parent: ?*Block, alloc: *Allocator) *Block {
-    var res = alloc.create(Block) catch unreachable;
-    res.* = Block {
-      .label = null,
-      .stmts = ArrayList(*Stmt).init(alloc),
-      .binds = StringHashMap(*Bind).init(alloc),
-      .parent = parent,
-    };
-    return res;
-  }
-  pub fn pushStmt(self: *Block, stmt: *Stmt) void {
-      self.stmts.append(stmt) catch unreachable;
-  }
-  pub fn pushBind(self: *Block, bind: *Bind) !void {
-      _ = self.binds.putNoClobber(bind.loc.lexeme, bind) catch return error.BindingAlreadyInScope;
-  }
-};
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Expr Stuff
-///////////////////////////////////////////////////////////////////////////////////////////////////
-pub const Expr = union(enum) {
-  Call: Call,
-  Literal: Token,
-  Symbol: Token,
-  Case: Case,
-  Tuple: ArrayList(*Expr),
-  pub fn fromLiteral(tok: Token, alloc: *Allocator) *Expr {
-    var res = alloc.create(Expr) catch unreachable;
-    res.* = Expr {
-      .Literal = tok,
-    };
-    return res;
-  }
-
-  pub fn fromSymbol(tok: Token, alloc: *Allocator) *Expr {
-
-    var res = alloc.create(Expr) catch unreachable;
-    res.* = Expr {
-      .Symbol = tok,
-    };
-    return res;
-  }
-
-  pub fn newTuple(alloc: *Allocator, first_val: *Expr) *Expr {
-    var res = alloc.create(Expr) catch unreachable;
-    res.* = Expr {
-      .Tuple = ArrayList(*Expr).init(alloc)
-    };
-    res.Tuple.append(first_val) catch unreachable;
-    return res;
-  }
-};
-
-// All operators are implemented as calls.
-// Examples:
-//  a + b => Call('+', 'a', 'b')
-//  a.b => Call('.', 'a', 'b')
-//  a(b, c, d) => Call('()', 'a', 'b', 'c', 'd')
-//  a.b(c) => Call('()', Call('.', 'a', 'b'), 'c')
-// I somewhat hope this more lispy style will allow for easier macro systems.
 pub const Call = struct {
-  func: []const u8,
-  args: ArrayList(*Expr),
-  
-  pub fn new(func: []const u8, alloc: *Allocator) *Expr {
-    var res = alloc.create(Expr) catch unreachable;
-    res.* = Expr {
-      .Call = Call {
-        .func = func,
-        .args = ArrayList(*Expr).init(alloc),
-      }
-    };
-
-    return res;
-  }
-
-  pub fn pushArg(self: *Call, arg: *Expr) void {
-    _ = self.args.append(arg) catch unreachable;
-  }
+    func: Token,
+    /// Since the majority of these are likely to be binary/unary expressions or function calls with
+    /// few args, prealloc 2 just in case.
+    args: SegmentedList(2, *Expr),
 };
 
-pub const CaseArm = struct {
-  cond: *Expr,
-  val: *Expr,
-};
-
+/// TODO: Case could really use a better name
 pub const Case = struct {
-  arms: ArrayList(CaseArm),
-  default: *Expr,
-
-  pub fn new(alloc: *Allocator) *Expr {
-    var res = alloc.create(Expr) catch unreachable;
-    res.* = Expr {
-      .Case = Case {
-        .arms = ArrayList(CaseArm).init(alloc),
-        .default = undefined,
-      }
-    };
-    return res;
-  }
+    arms: ArrayList(struct {
+        cond: *Expr,
+        val: *Expr,
+    }),
+    default: *Expr,
 };
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Type Stuff
-///////////////////////////////////////////////////////////////////////////////////////////////////
-pub const BaseType = union(enum) {
-  // Refers to another Bind somewhere
-  Symbol: Token,
-  Fn: FnType,
-  StructDef: StructDef,
-  EnumDef: EnumDef,
-  UnionDef: UnionDef,
-  // Zag will follow Nim's lead on concepts over interfaces
-  ConceptDef: ConceptDef,
-  VoidType: void,
-};
-
-pub const TypeMod = union(enum) {
-  Optional: void,
-  Slice: void,
-  Array: usize,
-};
-// TODO: Error types, a la zig
-pub const Type = struct {
-  mods: ArrayList(TypeMod),
-  base: BaseType,
-
-  pub fn pushOpt(self: *Type) void {
-    self.mods.append(TypeMod{ .Optional = {}}) catch unreachable;
-  }
-  pub fn pushSlice(self: *Type) void {
-    self.mods.append(TypeMod{.Slice = {}}) catch unreachable;
-  }
-  pub fn pushArray(self: *Type, size: usize) void {
-    self.mods.append(TypeMod{.Array = size}) catch unreachable;
-  }
-}; 
-
-pub const FnType = struct {
-  pure: bool,
-  args: ArrayList(*Bind),
-  // Zag does all returns with return value semantics
-  ret: *Bind,
-};
-
-pub const EnumDef = void; // TODO
-pub const StructDef = void; // TODO
-pub const UnionDef = void; // TODO
-pub const ConceptDef = void; // TODO
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// PrettyPrinter for the AST
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-pub const PrettyPrinter = struct {
-  level: usize = 0,
-  print: fn([]const u8)void,  
-
-  fn indent(self: PrettyPrinter) void {
-    var i: usize = 0;
-    while (i < level) : (i += 1) { self.print("    "); }
-  }
-  
-  pub fn prettyPrint(block: *Block, print: fn([]const u8)void) void {
-    var printer = PrettyPrinter { .level = 0, .print = print };
-    printer.printBlock(block);
-  }
-
-  pub fn printBlock(self: *PrettyPrinter, block: *Block) void {
-    self.print("Block {\n");
-    self.level += 1;
-
-    for (block.stmts.toSlice()) |stmt| {
-      self.printStmt(stmt);
-    }
-
-    self.level -= 1;
-    self.indent();
-    self.print("}\n");
-  }
-
-  pub fn printStmt(self: *PrettyPrinter, stmt: *Stmt) void {
-    switch (stmt.*) {
-      .If => |s| {
-        self.indent();
-        self.print("if ( ");
-        self.printExpr(s.cond);
-        self.print(" ) ");
-      },
-      .Loop => |s| {
-
-      },
-      .Bind => |s| {
-
-      },
-      .Block => |s| {
-
-      },
-      .Expr => |s| {
-
-      },
-    }
-  }
-}
