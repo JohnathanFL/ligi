52aac263caa326364b0c448dc8515c06f1fe1041
Johnathan Lee <jfl747076@gmail.com>
2020-11-26 15:48:56 -0600

    Work on prettifier

--- src/pretty.nim
+++ src/pretty.nim
@@ -1,1 +1,46 @@
+import streams
+
+import ast
+
+type Prettifier = object
+  curDent: int
+  stepDent: int
+  output: StringStream
+
+template write(a: untyped) =
+  p.output.write a
+
+proc indent(p: var Prettifier) =
+  p.curDent += p.stepDent
+proc dedent(p: var Prettifier) =
+  p.curDent -= p.stepDent
+proc writeDent(p: Prettifier) =
+  write '\n'
+  for i in 0..p.curDent:
+    write ' '
+
+proc pretty(p: var Prettifier, a: Atom) =
+  case a.kind:
+    of akList:
+      p.writeDent
+      write '('
+      p.indent
+      p.writeDent
+      for c in a.children:
+        p.pretty c
+      p.dedent
+      p.writeDent
+      write ')'
+    of akWord:
+      write a.id.lookup
+    of akStr:
+      write '"'
+      write a.str
+      write '"'
+    else: discard
+
+proc pretty*(a: Atom): string =
+  var prettifier = Prettifier(curDent: 0, stepDent: 2, output: newStringStream "")
+  prettifier.pretty a
+  return prettifier.output.readAll
 
